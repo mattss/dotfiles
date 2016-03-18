44991f0edc1d60aacdb862b6ebf6468f1906ce83
Matthew Sital-Singh <mattss@netsight.co.uk>
2016-03-18 11:44:39 +0000

    Util for editing as root

--- emacs/.emacs-custom.el
+++ emacs/.emacs-custom.el
@@ -37,6 +37,20 @@
 (add-hook 'python-mode-hook 'pylint-add-menu-items)
 ;;(add-hook 'python-mode-hook 'pylint-add-key-bindings)
 
+(defun sudo-edit (&optional arg)
+  "Edit currently visited file as root.
+
+Found at http://emacsredux.com/blog/2013/04/21/edit-files-as-root/
+
+With a prefix ARG prompt for a file to visit.
+Will also prompt for a file to visit if current
+buffer is not visiting a file."
+  (interactive "P")
+  (if (or arg (not buffer-file-name))
+      (find-file (concat "/sudo:root@localhost:"
+                         (ido-read-file-name "Find file(as root): ")))
+    (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
+
 (provide '.emacs-custom)
 ;;; .emacs-custom.el ends here
 
